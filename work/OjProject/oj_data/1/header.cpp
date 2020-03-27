0b39314f9c2693a1b88b055096d0a663f9df9a93
Centos7 <963835264.@qq.com>
2020-03-27 21:58:52 +0800

    在线OJ

---
+++ work/OjProject/oj_data/1/header.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+    public:
+        bool Find(int target, vector<vector<int> > array) {
+                    
+                
+        }
+
+};

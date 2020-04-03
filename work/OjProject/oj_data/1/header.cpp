c56306763448087d6a09f89f67d81a547a9e10d1
Centos7 <963835264.@qq.com>
2020-04-03 15:15:12 +0800

    OJ项目

--- work/OjProject/oj_data/1/header.cpp
+++ work/OjProject/oj_data/1/header.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
+#include <map>
+#include <algorithm>
+using namespace std;
 class Solution {
     public:
         bool Find(int target, vector<vector<int> > array) {
